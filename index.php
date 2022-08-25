33a1a4f03e9446a4fea5253fb5345eaecbcbbf01
painzhito <lucafbergamaschi@gmail.com>
2022-08-25 17:30:51 -0300

    prueba

--- index.php
+++ index.php
@@ -9,13 +9,17 @@ $cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
+//descomentar esto al commitearlo XDDDDDDD
 $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+//descomentar esto al commitearlo XDDDDDDD
 //conexion para probar una bd local
-//$conn = mysqli_connect('localhost','root','','pruebas');
+//$conn = mysqli_connect('localhost','root','','prueba');
+//conexion para probar una bd local
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="css/estilos.css">    
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -28,14 +32,6 @@ $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $c
             <td>email:</td>
             <td>ciudad:</td>
         </tr>
-        <p>hola ARMANDO</p>
-        <div class = "estilos">
-            <style>
-            </style>
-        </div>
-        <h1 class = "prueba">
-            probando aqui unas cosillas
-        </h1>
         <?php
             $sql="SELECT * from usuarios";
             $result=mysqli_query($conn, $sql);
@@ -53,7 +49,17 @@ $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $c
     
 </head>
 <body>
-    
+        <p>
+            hola ARMANDO
+        </p>
+        <div class = "estilos">
+            <style>
+            </style>
+        </div>
+        <h1 class = "prueba">
+            probando aqui unas cosillas
+        </h1>
+       
 </body>
 </html>
 
