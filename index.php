8b5ee46ca3a10858d530a55f59f5474af87d908e
ArmandoLun <armandogabrielluna@gmail.com>
2022-08-28 19:54:44 -0300

    pruebas boostrap

--- index.php
+++ index.php
@@ -18,15 +18,14 @@ $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $c
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <link rel="stylesheet" href="css/estilos.css">    
+<head> 
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>IntoBar42</title>
     
     <!-- External CSS -->
-    <link rel="stylesheet" href="./vendor/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <link rel="stylesheet" href="./vendor/select2.min.css">
     <link rel="stylesheet" href="./vendor/owl.carousel.min.css">
     <link rel="stylesheet" href="https://cdn.rawgit.com/noelboss/featherlight/1.7.13/release/featherlight.min.css">
