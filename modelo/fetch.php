2955d25911e0cf028a72331e0fe3b30ed2f55560
ArmandoLun <62290321+ArmandoLun@users.noreply.github.com>
2022-11-21 18:02:31 -0300

    probandofetch

---
+++ modelo/fetch.php
@@ -0,0 +1,23 @@
+<?php
+    if($_SERVER['REQUEST_METHOD'] == 'GET'){
+        
+        require_once("bd.php");
+
+        
+        
+        //usuarioprueba WHERE id = '$id'
+        $query = "SELECT * FROM menu";
+        
+        $result = $mysql -> query($query);
+
+        if ($mysql->affected_rows > 0){
+            while($row = $result->fetch_all(MYSQLI_ASSOC)){
+                $array = $row; //json
+            }
+            echo json_encode($array);
+        }else{
+            echo "No se encontro ningun usuario con ese ID";
+        }
+
+        $mysql->close(); 
+    }
