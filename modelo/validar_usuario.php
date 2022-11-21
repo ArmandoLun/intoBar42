ab7e4108fb925c7489229fd530749662c24e73ab
ArmandoLun <62290321+ArmandoLun@users.noreply.github.com>
2022-11-21 18:36:12 -0300

    b

--- modelo/validar_usuario.php
+++ modelo/validar_usuario.php
@@ -10,7 +10,7 @@ $sentencia -> execute();
 
 $resultado = $sentencia -> get_result();
 if($fila = $resultado -> fetch_assoc()){
-    echo " ", json_encode($fila, JSON_UNESCAPED_UNICODE), "\n";
+    echo json_encode($fila);
 }
 $sentencia -> close();
 $mysql -> close();
