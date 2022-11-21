250716af391594663d8cc45e533b51c7cd03b3aa
ArmandoLun <62290321+ArmandoLun@users.noreply.github.com>
2022-11-21 18:29:09 -0300

    cambiandoValidarUsuario

--- modelo/validar_usuario.php
+++ modelo/validar_usuario.php
@@ -10,7 +10,7 @@ $sentencia -> execute();
 
 $resultado = $sentencia -> get_result();
 if($fila = $resultado -> fetch_assoc()){
-    echo json_encode($fila, JSON_UNESCAPED_UNICODE);
+    echo json_encode($fila, JSON_UNESCAPED_UNICODE), "\n";
 }
 $sentencia -> close();
 $mysql -> close();
