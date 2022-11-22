3e6aa0bb0254b7a07542328903770c70ab38eda3
ArmandoLun <62290321+ArmandoLun@users.noreply.github.com>
2022-11-21 22:18:18 -0300

    d

--- modelo/fetch.php
+++ modelo/fetch.php
@@ -1,22 +1,11 @@
 <?php
-    if($_SERVER['REQUEST_METHOD'] == 'GET'){
+   if($_SERVER['REQUEST_METHOD'] == 'GET'){
         
         require_once("bd.php");
+
         
-        class foo
-        
-{
-    public $data = [];
-    function __construct($input)
-    {
-        $this->data = $input; 
-    }
-}
-        echo json_encode(new foo("qa"));
         
         //usuarioprueba WHERE id = '$id'
-        
-        /*
         $query = "SELECT * FROM menu";
         
         $result = $mysql -> query($query);
@@ -25,12 +14,11 @@
             while($row = $result->fetch_all(MYSQLI_ASSOC)){
                 $array = $row; //json
             }
-            echo json_encode(new foo($array));
+            echo json_encode($array);
         }else{
             echo "No se encontro ningun usuario con ese ID";
         }
 
         $mysql->close(); 
-        */
-        
     }
+
