46c4c623a4bfba890b88ae12e78d367569d86c6b
ArmandoLun <62290321+ArmandoLun@users.noreply.github.com>
2022-11-21 22:41:36 -0300

    probando

--- modelo/fetch.php
+++ modelo/fetch.php
@@ -2,10 +2,18 @@
    if($_SERVER['REQUEST_METHOD'] == 'GET'){
         
         require_once("bd.php");
-
+   class foo
         
-        echo (json_encode ($mysql->query("SELECT platillo, tiempo_coccion, precio, tipo, descripcion
-                FROM menu INNER JOIN tiposplatillos ON id_tiposPlatillos = tiposplatillos.id")->fetch_all(MYSQLI_ASSOC)));
+   {
+    public $data = [];
+    function __construct($input)
+    {
+        $this->data = $input; 
+    }
+   }
+        
+        echo (json_encode ((new foo(($mysql->query("SELECT platillo, tiempo_coccion, precio, tipo, descripcion
+                FROM menu INNER JOIN tiposplatillos ON id_tiposPlatillos = tiposplatillos.id")->fetch_all(MYSQLI_ASSOC))))));
         $mysql->close(); 
     }
 
