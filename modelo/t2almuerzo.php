9227d18f3ac5c0bc26795e30f8afdf6cff474420
ArmandoLun <62290321+ArmandoLun@users.noreply.github.com>
2022-11-22 00:16:52 -0300

    apis

---
+++ modelo/t2almuerzo.php
@@ -0,0 +1,37 @@
+<?php
+    if($_SERVER['REQUEST_METHOD'] == 'GET'){
+        
+        require_once("bd.php");
+
+        class foo
+        
+{
+    public $data = [];
+    function __construct($input)
+    {
+        $this->data = $input; 
+    }
+}
+
+        echo (json_encode ((new foo(($mysql->query("SELECT menu.id, platillo, tiempo_coccion, precio, tipo, descripcion, recomendar
+                FROM menu INNER JOIN tiposplatillos ON id_tiposPlatillos = tiposplatillos.id
+                WHERE tiposplatillos.id = 2")->fetch_all(MYSQLI_ASSOC))))));
+        $mysql->close(); 
+        /*
+        //usuarioprueba WHERE id = '$id'
+        $query = "SELECT * FROM menu";
+        
+        $result = $mysql -> query($query);
+
+        if ($mysql->affected_rows > 0){
+            while($row = $result->fetch_all(MYSQLI_ASSOC)){
+                $array = $row; //json
+            }
+            echo json_encode($array);
+        }else{
+            echo "No se encontro ningun usuario con ese ID";
+        }
+
+        $mysql->close(); 
+        */
+    }
